a669a1c148ab5dce39321a1b56dcaa8f431f201b
daniel360kim <nmrs.thrust@gmail.com>
2023-04-03 11:51:08 -0600

    removed angle wrapping. 0-360 ->  -180-180

--- software/sub_driver/src/Navigation/SensorFusion/Fusion.h
+++ software/sub_driver/src/Navigation/SensorFusion/Fusion.h
@@ -13,8 +13,9 @@ public:
     void update(LoggedData &data)
     {
         filter.MadgwickUpdate(data.rgyr.x, data.rgyr.y, data.rgyr.z, data.racc.x, data.racc.y, data.racc.z, data.rmag.x, data.rmag.y, data.rmag.z, data.delta_time);
-        data.rel_ori.x = Orientation::constrainAngle_whole(filter.getRoll());
-        data.rel_ori.y = Orientation::constrainAngle_whole(filter.getPitch());
+
+        data.rel_ori.x = filter.getRoll();
+        data.rel_ori.y = filter.getPitch();
         data.rel_ori.z = filter.getYaw();
     }
 
